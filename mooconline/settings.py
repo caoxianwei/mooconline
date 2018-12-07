300930d347ba1e51771d6f2539873f3d07b8d836
sunday <1756862146@qq.com>
2018-12-08 02:13:11 +0800

    created user database

--- mooconline/settings.py
+++ mooconline/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user.apps.UserConfig',
+    'users',
 ]
+AUTH_USER_MODEL = 'users.UserProfile'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
