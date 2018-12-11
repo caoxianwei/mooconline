4f3fe5f29e513974ce10ed0c42bf3ddba91dc857
sunday <1756862146@qq.com>
2018-12-11 23:23:09 +0800

    使用django 的views来继承view方法

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
 from .models import UserProfile
 
@@ -18,15 +19,34 @@ class CustomBackend(ModelBackend):
             return None
 
 
-def user_login(requst):
-    if requst.method == 'POST':
-        user_name = requst.POST.get('username', '')
-        pass_word = requst.POST.get('password', '')
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    def post(self, request):
+        user_name = request.POST.get('username', '')
+        pass_word = request.POST.get('password', '')
         user = authenticate(username=user_name, password=pass_word)
         if user is not None:
-            login(requst, user)
-            return render(requst, 'index.html')
+            login(request, user)
+            return render(request, 'index.html')
         else:
-            return render(requst, 'login.html', {'msg': '用户名和密码错误'})
-    elif requst.method == 'GET':
-        return render(requst, 'login.html')
+            return render(request, 'login.html', {'msg': '用户名和密码错误'})
+
+
+
+
+
+
+# 舍弃这个登录方法
+def user_login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('username', '')
+        pass_word = request.POST.get('password', '')
+        user = authenticate(username=user_name, password=pass_word)
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, 'login.html', {'msg': '用户名和密码错误'})
+    elif request.method == 'GET':
+        return render(request, 'login.html')
