31c442008d788c49869d36125d42579dd565ae15
sunday <1756862146@qq.com>
2018-12-11 21:58:16 +0800

    user login

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def user_login(requst):
+    if requst.method == 'POST':
+        user_name = requst.POST.get('username', '')
+        pass_word = requst.POST.get('password', '')
+        user = authenticate(username=user_name, password=pass_word)
+        if user is not None:
+            login(requst, user)
+            return render(requst, 'index.html')
+        else:
+            return render(requst, 'login.html')
+    elif requst.method == 'GET':
+        return render(requst, 'login.html')
--- mooconline/urls.py
+++ mooconline/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.urls import path
 from django.views.generic import TemplateView
 
+from users.views import user_login
+
 import xadmin
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    # path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('login/', user_login, name='login'),
 ]
