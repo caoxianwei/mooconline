3970be5eb62a072d41a6aa41a5997231a7ef529d
sunday <1756862146@qq.com>
2018-12-11 23:53:56 +0800

    添加forms做表单校验

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from django.views.generic.base import View
+from .forms import LoginForm
 
 from .models import UserProfile
+
 
 # Create your views here.
 
@@ -12,7 +14,7 @@ from .models import UserProfile
 class CustomBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = UserProfile.objects.get(Q(username=username)|Q(email=username))
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
             if user.check_password(password):
                 return user
         except Exception as e:
@@ -22,19 +24,21 @@ class CustomBackend(ModelBackend):
 class LoginView(View):
     def get(self, request):
         return render(request, 'login.html')
+
     def post(self, request):
-        user_name = request.POST.get('username', '')
-        pass_word = request.POST.get('password', '')
-        user = authenticate(username=user_name, password=pass_word)
-        if user is not None:
-            login(request, user)
-            return render(request, 'index.html')
+        # 实例化
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user_name = request.POST.get('username', '')
+            pass_word = request.POST.get('password', '')
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                login(request, user)
+                return render(request, 'index.html')
+            else:
+                return render(request, 'login.html', {'msg': '用户名和密码错误'})
         else:
-            return render(request, 'login.html', {'msg': '用户名和密码错误'})
-
-
-
-
+            return render(request, 'login.html', {'login_form': login_form})
 
 
 # 舍弃这个登录方法
