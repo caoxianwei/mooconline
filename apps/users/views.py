f255e196e75391fa1aaf025a868e09990a4daf47
sunday <1756862146@qq.com>
2018-12-16 02:18:44 +0800

    忘记密码 && 重置

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
+from .models import EmailVerifyRecord
 from django.db.models import Q
 from django.views.generic.base import View
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, ForgetPwdForm
 from django.contrib.auth.hashers import make_password
 from utils.email_send import send_register_eamil
 
@@ -35,8 +36,11 @@ class LoginView(View):
             pass_word = request.POST.get('password', '')
             user = authenticate(username=user_name, password=pass_word)
             if user is not None:
-                login(request, user)
-                return render(request, 'index.html')
+                if user.is_active:
+                    login(request, user)
+                    return render(request, 'index.html')
+                else:
+                    return render(request, 'login.html', {'msg': '用户未激活'})
             else:
                 return render(request, 'login.html', {'msg': '用户名和密码错误'})
         else:
@@ -57,11 +61,24 @@ def user_login(request):
     elif request.method == 'GET':
         return render(request, 'login.html')
 
+
 # 激活用户
 class ActiveUserView(View):
     def get(self, request, active_code):
-        pass
-
+        all_records = EmailVerifyRecord.objects.filter(code=active_code)
+        if all_records:
+            for record in all_records:
+                # 获取到对应的邮箱
+                email = record.email
+                # 查找到邮箱对应的user
+                user = UserProfile.objects.get(email=email)
+                user.is_active = True
+                user.save()
+        # 验证码不对的时候跳转到激活失败页面
+        else:
+            return render(request, 'active_fail.html')
+        # 激活成功跳转到登录页面
+        return render(request, "login.html", )
 
 
 # 注册
@@ -74,6 +91,8 @@ class RegisterView(View):
         register_form = RegisterForm()
         if register_form.is_valid():
             user_name = request.POST.get('email', None)
+            if UserProfile.objects.filter(email=user_name):
+                return render(request, 'register.html', {'register_form': register_form})
             pass_word = request.POST.get('password', '')
             user_profile = UserProfile()
             user_profile.username = user_name
@@ -85,3 +104,31 @@ class RegisterView(View):
             return render(request, 'login.html')
         else:
             return render(request, 'register.html', {'register_form': register_form})
+
+
+class ForgetPwdView(View):
+    def get(self, request):
+        forget_form = ForgetPwdForm()
+        return render(request, 'forgetpwd.html', {'forget_form': forget_form})
+
+    def post(self, request):
+        forget_form = ForgetPwdForm(request)
+        if forget_form.is_valid():
+            email = request.POST.get('email', '')
+            send_register_eamil(email, 'forget')
+            return render(request, 'send_success.html')
+        else:
+            return render(request, 'forgetpwd.html', {'forget_form': forget_form})
+
+
+class ResetView(View):
+    def get(self, request, active_code):
+        all_records = EmailVerifyRecord.objects.filter(code=active_code)
+        if all_records:
+            for record in all_records:
+                email = record.email
+                return render(request, 'password_reset.html', {'email': email})
+        else:
+            return render(request, 'active_fail.html')
+        return render(request, "login.html", )
+
--- mooconline/urls.py
+++ mooconline/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
-from users.views import LoginView, RegisterView, ActiveUserView
+from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView
 
 import xadmin
 
@@ -28,6 +28,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('captcha/', include('captcha.urls')),
-
     re_path('active/(?P<active_code>.*)/', ActiveUserView.as_view(), name='user_active'),
+    path('forget/', ForgetPwdView.as_view(), name='forget_pwd'),
+    re_path('reset/(?P<active_code>.*)/', ResetView.as_view(), name='reset_pwd'),
 ]
